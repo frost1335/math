8365b049e9e2180045983167ee553b9f10e241c9
frost1335 <dilrozbek.developer@gmail.com>
2024-02-14 18:25:05 +0500

    fix: jpg to webp in assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,5 @@
-import HeroImg1 from './DSC00266.jpg'
-import HeroImg2 from './DSC00276.jpg'
+import HeroImg1 from './DSC00266.webp'
+import HeroImg2 from './DSC00276.webp'
 import Teacher1 from './465456.jpg'
 import Teacher2 from './IMG_5085-min.jpg'
 
