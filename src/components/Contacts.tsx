b05265ae332dd5c7eaf67896793d68b69031567b
dilrozbek <65526165+frost1335@users.noreply.github.com>
2024-03-14 15:02:27 +0500

    Update Contacts socials

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,8 +1,8 @@
 import { BsTelegram } from "react-icons/bs";
 import { FaTelegramPlane } from "react-icons/fa";
 import { FaPhone } from "react-icons/fa6";
-import { MdMail } from "react-icons/md";
 import { RiInstagramFill } from "react-icons/ri";
+import { INSTAGRAM_LINK, PHONE_LINK, TELEGRAM_LINK } from "@/constants";
 
 export default function Contacts() {
     return (
@@ -15,7 +15,7 @@ export default function Contacts() {
             </h2>
             <ul className="grid lg:grid-cols-4 xl:gap-6 gap-4 gap-y-8 sm:grid-cols-2 mt-6">
                 <li>
-                    <a href="" className="flex flex-col items-center text-slate-600 hover:text-red-600 transition-all">
+                    <a target="_blank" href={TELEGRAM_LINK} className="flex flex-col items-center text-slate-600 hover:text-red-600 transition-all">
                         <span className="mb-4 text-2xl md:text-4xl w-16 h-16 md:w-20 md:h-20 flex items-center justify-center rounded-full border-2 border-zinc-700 text-zinc-700 shadow-lg hover:text-white hover:bg-red-600 hover:border-red-600 hover:shadow-xl transition-all">
                             <BsTelegram />
                         </span>
@@ -23,7 +23,7 @@ export default function Contacts() {
                     </a>
                 </li>
                 <li>
-                    <a href="" className="flex flex-col items-center text-slate-600 hover:text-red-600 transition-all">
+                    <a target="_blank" href={INSTAGRAM_LINK} className="flex flex-col items-center text-slate-600 hover:text-red-600 transition-all">
                         <span className="mb-4 text-2xl md:text-4xl w-16 h-16 md:w-20 md:h-20 flex items-center justify-center rounded-full border-2 border-zinc-700 text-zinc-700 shadow-lg hover:text-white hover:bg-red-600 hover:border-red-600 hover:shadow-xl transition-all">
                             <RiInstagramFill />
                         </span>
@@ -31,19 +31,11 @@ export default function Contacts() {
                     </a>
                 </li>
                 <li>
-                    <a href="" className="flex flex-col items-center text-slate-600 hover:text-red-600 transition-all">
+                    <a target="_blank" href={PHONE_LINK} className="flex flex-col items-center text-slate-600 hover:text-red-600 transition-all">
                         <span className="mb-4 text-2xl md:text-4xl w-16 h-16 md:w-20 md:h-20 flex items-center justify-center rounded-full border-2 border-zinc-700 text-zinc-700 shadow-lg hover:text-white hover:bg-red-600 hover:border-red-600 hover:shadow-xl transition-all">
                             <FaPhone />
                         </span>
                         <p className="font-medium font-mono">+998 93 189 73 18</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="" className="flex flex-col items-center text-slate-600 hover:text-red-600 transition-all">
-                        <span className="mb-4 text-2xl md:text-4xl w-16 h-16 md:w-20 md:h-20 flex items-center justify-center rounded-full border-2 border-zinc-700 text-zinc-700 shadow-lg hover:text-white hover:bg-red-600 hover:border-red-600 hover:shadow-xl transition-all">
-                            <MdMail />
-                        </span>
-                        <p className="font-medium font-mono">innovationmatkematika@mail.ru</p>
                     </a>
                 </li>
             </ul>
